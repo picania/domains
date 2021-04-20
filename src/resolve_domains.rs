a4b0e102d59aa324e6435c13421668c976cfbff6
picania <mangustspam@gmail.com>
2021-04-20 13:13:57 +0700

    Исправлены предупреждения линтера. Автоматический реформат

--- src/resolve_domains.rs
+++ src/resolve_domains.rs
@@ -1,9 +1,9 @@
-use std::fs::{self, File};
-use std::io::{self, BufReader, BufRead, BufWriter, Write};
 use flate2::read::GzDecoder;
-use trust_dns_resolver::Resolver;
-use trust_dns_resolver::config::*;
+use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::net::IpAddr;
+use trust_dns_resolver::config::*;
+use trust_dns_resolver::Resolver;
 
 // Root servers located in Russian Federation
 const E_ROOT_V4: [u8; 4] = [192, 203, 230, 10];
@@ -16,7 +16,7 @@ const I_ROOT_V4: [u8; 4] = [192, 36, 148, 17];
 // I IPv6	2001:7fe::53
 const K_ROOT_V4: [u8; 4] = [193, 0, 14, 129];
 // IPv6	2001:7fd::1
-const L_ROOT_V4: [u8; 4]= [199, 7, 83, 42];
+const L_ROOT_V4: [u8; 4] = [199, 7, 83, 42];
 // IPv6	2001:500:9f::42
 
 fn create_resolver() -> io::Result<Resolver> {
@@ -29,10 +29,12 @@ fn create_resolver() -> io::Result<Resolver> {
         IpAddr::V4(L_ROOT_V4.into()),
     ];
     let group = NameServerConfigGroup::from_ips_clear(&ips, 53);
-    let mut options = ResolverOpts::default();
-    options.ip_strategy = LookupIpStrategy::Ipv4Only;
-    options.attempts = ips.len();
-    options.rotate = true;
+    let options = ResolverOpts {
+        ip_strategy: LookupIpStrategy::Ipv4Only,
+        attempts: ips.len(),
+        rotate: true,
+        ..ResolverOpts::default()
+    };
     let config = ResolverConfig::from_parts(None, vec![], group);
 
     Resolver::new(config, options)
@@ -43,13 +45,12 @@ fn extract_domains(from: &str, to: &str) -> io::Result<()> {
     let output = File::create(to)?;
     let decoder = GzDecoder::new(input);
     let reader = BufReader::new(decoder);
-    let mut writer= BufWriter::new(output);
+    let mut writer = BufWriter::new(output);
 
-    reader.lines()
+    reader
+        .lines()
         .filter_map(|x| x.ok())
-        .map(|s| {
-            s.split_ascii_whitespace().take(1).collect::<String>()
-        })
+        .map(|s| s.split_ascii_whitespace().take(1).collect::<String>())
         .for_each(|domain| {
             writer.write_fmt(format_args!("{}\n", domain)).unwrap();
         });
@@ -65,25 +66,24 @@ fn resolve_domains(unresolved: &str, resolved: &str) -> io::Result<()> {
     let mut writer = BufWriter::new(output);
     let mut unr_writer = BufWriter::new(File::create("zones/unresolved.ru.tmp")?);
 
-    reader.lines()
+    reader
+        .lines()
         .filter_map(|x| x.ok())
         .take(1000)
-        .for_each(|domain| {
-            match resolver.lookup_ip(&domain) {
-                Ok(ips) => {
-                    writer.write_all(domain.as_bytes()).unwrap();
-                    print!("{}", &domain);
-                    for ip in ips {
-                        writer.write_fmt(format_args!(" {}", &ip)).unwrap();
-                        print!(" {}", &ip);
-                    }
-                    writer.write_all(b"\n").unwrap();
-                    println!();
-                    io::stdout().flush().unwrap();
-                },
-                Err(_) => {
-                    unr_writer.write_fmt(format_args!("{}\n", domain)).unwrap();
-                },
+        .for_each(|domain| match resolver.lookup_ip(&domain) {
+            Ok(ips) => {
+                writer.write_all(domain.as_bytes()).unwrap();
+                print!("{}", &domain);
+                for ip in ips {
+                    writer.write_fmt(format_args!(" {}", &ip)).unwrap();
+                    print!(" {}", &ip);
+                }
+                writer.write_all(b"\n").unwrap();
+                println!();
+                io::stdout().flush().unwrap();
+            }
+            Err(_) => {
+                unr_writer.write_fmt(format_args!("{}\n", domain)).unwrap();
             }
         });
 
